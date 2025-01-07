aad60bab02d2416d8b8c8101c12f8156df8e1c42
VitorDev123 <VitorDevmagalhaes@gmail.com>
2025-01-07 09:01:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+Console.WriteLine("Operações: Soma = +, Subtração = -, Divisão = /, Multiplicação = *");
+
+double? n1, n2;
+
+Console.WriteLine("Digite um numero ");
+n1 = double.Parse(Console.ReadLine());
+
+string? operação;
+
+Console.WriteLine("Digite uma operação ");
+operação = (Console.ReadLine());
+
+Console.WriteLine("Digite um numero ");
+n2 = double.Parse(Console.ReadLine());
+
+switch(operação)
+{
+    //Soma
+
+    case "+":
+
+     Console.WriteLine(n1 + n2);
+
+    break;
+    //Subtração
+
+    case "-":
+
+     Console.WriteLine(n1 - n2);
+
+    break;
+
+    //Divisão
+     case "/":
+
+     Console.WriteLine(n1 / n2);
+
+    break;
+
+    //Multiplicação
+
+    case "*":
+
+     Console.WriteLine(n1 * n2);
+
+    break;
+
+    default:
+        Console.WriteLine("Operação invalida ");
+        return;
+}
